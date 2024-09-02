fcadd33960510031bbd2779ac7909b5cf57ec462
Jayy5121 <142887196+Jayy5121@users.noreply.github.com>
2024-09-02 08:13:23 +0530

    Add files via upload

---
+++ ecell site/app.js
@@ -0,0 +1,10 @@
+var igbutton=document.getElementById("igbutton");
+igbutton.addEventListener("click",function(){
+    window.open("https://www.instagram.com/ecell-gcoe_kolhapur","_blank");
+    window.close();
+})
+var xbutton=document.getElementById("xbutton");
+xbutton.addEventListener("click",function(){
+    window.open("https://www.x.com/ECell_GCOEK","_blank");
+    window.close();
+})
