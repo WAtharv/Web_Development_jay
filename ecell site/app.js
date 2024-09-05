ea9ba497997ab09961e144361da2d2b0f3ea9c58
Jayy5121 <142887196+Jayy5121@users.noreply.github.com>
2024-09-05 09:07:43 +0530

    uploaded all files

---
+++ ecell site/app.js
@@ -0,0 +1,27 @@
+var igbutton=document.getElementById("igbutton");
+igbutton.addEventListener("click",function(){
+    window.open("https://www.instagram.com/ecell_gcoe_kolhapur/","_blank");
+    
+})
+var xbutton=document.getElementById("xbutton");
+xbutton.addEventListener("click",function(){
+    window.open("https://www.x.com/ECell_GCOEK/","_blank");
+})
+var igbutton=document.getElementById("igbutton2");
+igbutton.addEventListener("click",function(){
+    window.open("https://www.instagram.com/ecell_gcoe_kolhapur/","_blank");
+    
+})
+var xbutton=document.getElementById("xbutton2");
+xbutton.addEventListener("click",function(){
+    window.open("https://www.x.com/ECell_GCOEK/","_blank");
+})
+var typed = new Typed('#element',
+    {
+      strings: ['Where Creativity, Meets Commerce'],
+      typeSpeed: 60,
+    });
+var galbtn=document.getElementById("galbtn");
+galbtn.addEventListener("click",function(){
+    window.location.href="gallery.html";
+})
